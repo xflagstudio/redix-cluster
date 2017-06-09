d239d53943b0a10bd5a05eeb0ebf2c3670ab2b7e
Masahiro Tokioka <iede.nyanko@gmail.com>
2017-06-09 16:00:42 +0900

    fix parse trans result

--- lib/redix_cluster/run.ex
+++ lib/redix_cluster/run.ex
@@ -127,6 +127,10 @@ defmodule RedixCluster.Run do
     RedixCluster.Monitor.refresh_mapping(version)
     {:error, :retry}
   end
+  defp parse_trans_result({:error, %Redix.ConnectionError{}}, {version, _pool_name}, _command, _type, _opts) do
+    RedixCluster.Monitor.refresh_mapping(version)
+    {:error, :retry}
+  end
   defp parse_trans_result(payload, _, _, _, _), do: payload
 
   defp verify_command_key(term1, term2) do
