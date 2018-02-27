dee26af1706abf20112382d0223eaa5f5d465d00
Masahiro Tokioka <iede.nyanko@gmail.com>
2018-02-27 12:16:54 +0900

    relax dependency to redix

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule RedixCluster.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [ {:redix, "~> 0.6.0"},
+    [ {:redix, ">= 0.6.0"},
       {:poolboy, "~> 1.5", override: true},
       {:dialyze, "~> 0.2", only: :dev},
       {:dogma, "~> 0.0", only: :dev},
