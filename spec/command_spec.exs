b0ff0efe9f1d76a95e05a19d66e82fb0ebb6962a
neglect-yp <grandis.hebitombo@gmail.com>
2020-10-19 17:12:14 +0900

    fix: warnings

--- spec/command_spec.exs
+++ spec/command_spec.exs
@@ -2,9 +2,9 @@ defmodule RedixCluster.Command.Spec do
   use ESpec
 
   before do
-    allow Redix |> to accept :start_link, fn(_) -> {:ok, self()} end, [:non_strict, :unstick]
-    allow Redix |> to accept :stop, fn(_) -> :ok end, [:non_strict, :unstick]
-    allow Redix |> to accept :command, fn
+    allow(Redix) |> to(accept(:start_link, fn(_) -> {:ok, self()} end, [:non_strict, :unstick]))
+    allow(Redix) |> to(accept(:stop, fn(_) -> :ok end, [:non_strict, :unstick]))
+    allow(Redix) |> to(accept(:command, fn
       (_, ~w(CLUSTER SLOTS), _) -> {:ok,
                                  [[10923, 16383, ["10.1.2.5", 7000], ["10.1.2.5", 7001]],
                                   [5461, 10922, ["10.1.2.6", 7000], ["10.1.2.7", 7001]],
@@ -13,7 +13,7 @@ defmodule RedixCluster.Command.Spec do
       (_, ~w(flushdb), _) -> {:ok, "OK"}
       (_, ~w(get a), _) -> {:ok, "test"}
       (_, ~w(incr a), _) -> {:error, %Redix.Error{message: "ERR value is not an integer or out of range"}}
-    end, [:non_strict, :unstick]
+    end, [:non_strict, :unstick]))
 
     Application.ensure_all_started(:redix_cluster)
     {:shared, count: 1} #saves {:key, :value} to `shared`
@@ -22,21 +22,21 @@ defmodule RedixCluster.Command.Spec do
   finally do: {:shared, answer: shared.count + 1}
 
   context "command test" do
-    it do: expect RedixCluster.command(~w(set a test)) |> to eq {:ok, "OK"}
-    it do: expect RedixCluster.command(~w(get a)) |> to eq {:ok, "test"}
-    it do: expect RedixCluster.command(~w(incr a)) |> to eq {:error, %Redix.Error{message: "ERR value is not an integer or out of range"}}
+    it(do: expect(RedixCluster.command(~w(set a test))) |> to(eq({:ok, "OK"})))
+    it(do: expect(RedixCluster.command(~w(get a))) |> to(eq({:ok, "test"})))
+    it(do: expect(RedixCluster.command(~w(incr a))) |> to(eq({:error, %Redix.Error{message: "ERR value is not an integer or out of range"}})))
   end
 
   context "command! test" do
-    it do: expect RedixCluster.command!(~w(set a test)) |> to eq "OK"
-    it do: expect RedixCluster.command!(~w(get a)) |> to eq "test"
+    it(do: expect(RedixCluster.command!(~w(set a test))) |> to(eq("OK")))
+    it(do: expect(RedixCluster.command!(~w(get a))) |> to(eq("test")))
     it do
       action = fn() -> RedixCluster.command!(~w(incr a)) end
-      expect  action |> to raise_exception Redix.Error, "ERR value is not an integer or out of range"
+      expect(action) |> to(raise_exception(Redix.Error, "ERR value is not an integer or out of range"))
     end
   end
 
   context "flushdb test" do
-    it do: expect RedixCluster.flushdb() |> to eq {:ok, "OK"}
+    it(do: expect RedixCluster.flushdb() |> to(eq({:ok, "OK"})))
   end
 end
--- spec/hashslot_spec.exs
+++ spec/hashslot_spec.exs
@@ -6,13 +6,13 @@ defmodule CreateHashSlot.Spec do
           "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
   it  do
-    chars_hash() |> to eq [15495, 3300, 7365, 11298, 15363, 3168, 7233, 11694, 15759, 3564, 7629, 11562,
+    chars_hash() |> to(eq([15495, 3300, 7365, 11298, 15363, 3168, 7233, 11694, 15759, 3564, 7629, 11562,
                          15627, 3432, 7497, 16023, 11958, 7893, 3828, 15891, 11826, 7761, 3696, 16287,
-                         12222, 8157]
+                         12222, 8157]))
   end
 
   it do
-    same_hash() |> to eq List.duplicate(15495, 100)
+    same_hash() |> to(eq(List.duplicate(15495, 100)))
   end
 
   defp chars_hash() do
